456a1108eef35b9779ca6319b39846369ade97aa
isuy <isuy0704@gmail.com>
2015-06-26 23:22:19 +0800

    Add jQuery

--- static/js/isuy.js
+++ static/js/isuy.js
@@ -1,5 +1,26 @@
-window.onload = function() {
+var stopAllAudio = function() {
+  $('.audio').each(function() {
+    this.pause();
+  })
+}
+
+var updateElapsedTime = function() {
+  var date = new Date();
+  $('#elapsed-time').text(date.toLocaleString());
+}
+
+$(document).ready(function() {
   var container = document.getElementById('container');
   var pages = document.querySelectorAll('.page');
-  var slip = Slip(container, 'y').webapp(pages);
-}
+  var slip = Slip(container, 'y').webapp(pages).end(function() {
+    if (this.page == 1) {
+      stopAllAudio();
+      $('#page-2-audio').trigger('play');
+    } else if (this.page == 3) {
+      stopAllAudio();
+      $('#page-4-audio').trigger('play');
+    }
+  });
+
+  setInterval(updateElapsedTime, 200);
+})
