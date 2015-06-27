9137f9bb73c63970412f83efb1f70f1f7635dbbb
isuy <isuy0704@gmail.com>
2015-06-27 15:11:46 +0800

    Delete audio

--- static/js/isuy.js
+++ static/js/isuy.js
@@ -1,7 +1,33 @@
-var stopAllAudio = function() {
+var stopAllAudio = function(excludedID) {
   $('.audio').each(function() {
-    this.pause();
+    var currentID = this.getAttribute('id');
+    if (currentID != excludedID && !this.paused)
+      this.pause();
   })
+}
+
+var playAudio = function(id) {
+  var audio = $('#' + id);
+  cover = audio.parent('.audio-container').find('.cover');
+
+  audio.trigger('play');
+  if (!cover.hasClass('animate'))
+    cover.addClass('animate');
+}
+
+var playOrPauseAudio = function(cover) {
+  var audio = cover.parent('.audio-container').find('audio');
+  var paused = audio.prop('paused');
+
+  if (paused) {
+    audio.trigger('play');
+    if (!cover.hasClass('animate'))
+      cover.addClass('animate');
+  } else {
+    audio.trigger('pause');
+    if (cover.hasClass('animate'))
+      cover.removeClass('animate');
+  }
 }
 
 var updateElapsedTime = function() {
@@ -13,14 +39,23 @@ $(document).ready(function() {
   var container = document.getElementById('container');
   var pages = document.querySelectorAll('.page');
   var slip = Slip(container, 'y').webapp(pages).end(function() {
+    if (this.orient.toString() == '')
+      return;
+
     if (this.page == 1) {
-      stopAllAudio();
-      $('#page-2-audio').trigger('play');
+      id = 'page-2-audio';
+      stopAllAudio(id);
+      playAudio(id);
     } else if (this.page == 3) {
-      stopAllAudio();
-      $('#page-4-audio').trigger('play');
+      id = 'page-4-audio';
+      stopAllAudio(id);
+      playAudio(id);
     }
   });
 
   setInterval(updateElapsedTime, 200);
+
+  $('.cover').click(function() {
+    playOrPauseAudio($(this));
+  });
 })
